de77a75e49eda68c672e3f16a8a49d8631172e20
YuanSun <reggie.sunyuan@me.com>
2017-01-04 19:28:19 -0500

    Adjastment

---
+++ LeetCode2/src/reconstructItinerary/FindItinerary.java
@@ -0,0 +1,19 @@
+package reconstructItinerary;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * @author ysun
+ */
+public class FindItinerary {
+    public static HashMap<String, PriorityQueue<String>>map = new HashMap<>();
+    public static LinkedList<String> result = new LinkedList<>();
+    
+    public static List<String> findItineary(String[][] tickets){
+        // put all tickets to the map. "From" as the key, "To"s are put in the corresponding Queue
+    }
+}
